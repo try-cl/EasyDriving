63da7893cd3ff5dc48057c4734ebc70c50a3c13c
Hexing13 <vicky1776898728@gmail.com>
2015-12-11 21:58:36 +0800

    初次框架

---
+++ src/main/java/com/easyDriving/dao/HelloDaoImp.java
@@ -0,0 +1,11 @@
+package com.easyDriving.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by hexing on 15-12-11.
+ */
+@Repository
+public class HelloDaoImp implements HelloDao{
+
+}
