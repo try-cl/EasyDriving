f3d4f7342fa11a817780ecf87c0b31d401a32be2
Hexing13 <vicky1776898728@gmail.com>
2015-12-15 22:22:13 +0800

    合并露露第一次提交的东西并修改他

--- src/main/java/com/easyDriving/mapper/MailMapper.java
+++
@@ -1,8 +0,0 @@
-package com.easyDriving.mapper;
-
-/**
- * Created by chailu on 2015/12/13.
- */
-public interface MailMapper {
-
-}
--- src/main/java/com/easyDriving/pojo/Student.java
+++
@@ -1,7 +0,0 @@
-package com.easyDriving.pojo;
-
-/**
- * Created by chailu on 2015/12/12.
- */
-public class Student {
-}
